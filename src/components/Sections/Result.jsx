d4906928de9092c9d7efba6c87eef8aaf51fa9a9
Alex Modugno <AlexModugno2024@u.northwestern.edu>
2023-05-21 13:17:18 -0500

    Results logic

--- src/components/Sections/Result.jsx
+++ src/components/Sections/Result.jsx
@@ -1,20 +1,42 @@
 import React, { useState, useEffect, useContext } from 'react';
 import AssessmentContext from '../../helpers/Contexts';
+import { ResultDetails } from '../../helpers/RecommendedDetails';
 import Header from '../Header';
 import Providers from '../Recommend/Providers';
 import { Background, Content } from '../../styles';
 
 const Result = () => {
   const { answers } = useContext(AssessmentContext);
-  [resultIndex, setResultIndex] = useState([]) 
-  if(answers["06"].includes("SHOOTING PAIN")) {
-      setResultIndex([...resultIndex, 0])
-  }
-  if(answers["07"].includes("LEAN FORWARD") && (!answers["07"].includes("AM AT REST")) || (!answers["07"].includes("LEAN BACK"))) {
-    setResultIndex([...resultIndex, 1])
-  }
+  const [resultIndex, setResultIndex] = useState([]);
+  // [resultIndex, setResultIndex] = useState([]);
+  // if (answers['06'].includes('SHOOTING PAIN')) {
+  //   setResultIndex([...resultIndex, 0]);
+  // }
+  // if (
+  //   (answers['07'].includes('LEAN FORWARD') &&
+  //     !answers['07'].includes('AM AT REST')) ||
+  //   !answers['07'].includes('LEAN BACK')
+  // ) {
+  //   setResultIndex([...resultIndex, 1]);
+  // }
+  // for (let i = 0; i < resultIndex.length; i++) {
+  //   console.log(`diagnosis ${i}`, ResultDetails[i]);
+  // }
   useEffect(() => {
-    console.log('answers:', answers);
+    console.log('answers first', answers);
+    if (answers['06'].includes('SHOOTING PAIN')) {
+      setResultIndex([...resultIndex, 0]);
+    }
+    if (
+      (answers['07-subq-0'].includes('LEAN FORWARD') &&
+        !answers['07-subq-0'].includes('AM AT REST')) ||
+      !answers['07-subq-0'].includes('LEAN BACK')
+    ) {
+      setResultIndex([...resultIndex, 1]);
+    }
+    for (let i = 0; i < resultIndex.length; i++) {
+      console.log(`diagnosis ${i}`, ResultDetails[i]);
+    }
   }, [answers]);
   console.log('answers:', answers);
   return (
