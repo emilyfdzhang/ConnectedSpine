46e00fcaf6b1d09b15c1eb26abad7bf0a9e17e74
Alex Modugno <AlexModugno2024@u.northwestern.edu>
2023-05-22 09:31:44 -0500

    Add provider info, can change later

--- src/components/Recommend/Providers.jsx
+++ src/components/Recommend/Providers.jsx
@@ -32,7 +32,56 @@ const Providers = ({ resultIndex }) => {
       <Recomendation>
         <Recomended>Recommended providers</Recomended>
         <Cards>
-          <InfoCard
+          {diagnosis.includes('Metastatic disease') ||
+          diagnosis.includes('Infection') ||
+          diagnosis.includes('Cauda Equina') ||
+          diagnosis.includes('Myocardial Ischemia') ? (
+            <InfoCard
+              icon={faPerson}
+              provider={'ER/Urgent Care'}
+              bold={'90% of people'}
+              text={' with your answers seek care from the ER or urgent care'}
+            />
+          ) : null}
+          {diagnosis.includes('Facet Arthropathy') ? (
+            <InfoCard
+              icon={faPerson}
+              provider={'Primary Care Physician'}
+              bold={'90% of people'}
+              text={
+                ' with your answers seek care from a primary care physician'
+              }
+            />
+          ) : null}
+          {diagnosis.includes('Myelopathy') ||
+          diagnosis.includes('Lumbar Stenosis') ? (
+            <InfoCard
+              icon={faPerson}
+              provider={'Neurosurgeon'}
+              bold={'90% of people'}
+              text={' with your answers seek care from a neurosurgeon'}
+            />
+          ) : null}
+          {diagnosis.includes('Myelopathy') ? (
+            <InfoCard
+              icon={faPerson}
+              provider={'Neurologist'}
+              bold={'90% of people'}
+              text={' with your answers seek care from a neurologist'}
+            />
+          ) : null}
+          {diagnosis.includes('Lumbar Radiculopathy') ||
+          diagnosis.includes('Facet Arthropathy') ? (
+            <InfoCard
+              icon={faPerson}
+              provider={'Interventional Pain Physician'}
+              bold={'90% of people'}
+              text={
+                ' with your answers seek care from an interventional pain physician'
+              }
+            />
+          ) : null}
+          {/* <InfoCard
             icon={faPerson}
             provider={'Physical Therapist'}
             bold={'90% of people'}
@@ -43,7 +92,7 @@ const Providers = ({ resultIndex }) => {
             provider={'Physiatrist (MD)'}
             bold={'35% of people'}
             text={' with your answers seek care from a Physiatrist'}
-          />
+          /> */}
         </Cards>
       </Recomendation>
       <Save>
