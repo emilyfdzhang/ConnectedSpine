c7f9b191545fe8f6af2d4d4f341a716b6e1ecaa2
Alex Modugno <AlexModugno2024@u.northwestern.edu>
2023-05-22 10:35:03 -0500

    First test passes

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,17 +1,29 @@
 import { describe, expect, test } from 'vitest';
 import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
+import { useDbData } from './utilities/firebase';
+vi.mock('./utilities/firebase');
 
-describe('counter tests', () => {
-  test('Counter should be 0 at the start', () => {
-    render(<App />);
-    expect(screen.getByText('count is: 0')).toBeDefined();
+describe('initial tests', () => {
+  const mockData = {
+    '00': 'result',
+  };
+
+  beforeEach(() => {
+    useDbData.mockReturnValue([mockData, false, null]);
   });
 
-  test('Counter should increment by one when clicked', async () => {
+  test('Home page shows up when finished loading', () => {
     render(<App />);
-    const counter = screen.getByRole('button');
-    fireEvent.click(counter);
-    expect(await screen.getByText('count is: 1')).toBeDefined();
+    expect(
+      screen.getByText('Experiencing pain or discomfort in your back or neck?')
+    ).toBeDefined();
   });
+
+  // test('Counter should increment by one when clicked', async () => {
+  //   render(<App />);
+  //   const counter = screen.getByRole('button');
+  //   fireEvent.click(counter);
+  //   expect(await screen.getByText('count is: 1')).toBeDefined();
+  // });
 });
